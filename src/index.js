7ffcbc64bac97e4e70f114ff2e48696a2c12107a
Logan Elliott <logansails@gmail.com>
2017-06-02 09:43:07 +1200

    Add material UI. Add scss post processing. Add .eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.scss';
+import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App/>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
