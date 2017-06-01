1536e32d42f3baeaebc8f0b121189771555a8448
Logan Elliott <logansails@gmail.com>
2017-06-02 11:19:45 +1200

    Add colour picker dialog

---
+++ src/colourDialog/colourDialog.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import { BlockPicker } from 'react-color';
+
+const propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired,
+};
+
+class ColourDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colour: 'red',
+        };
+    }
+
+    handleChangeComplete = (colour, event) => {
+        console.log(colour);
+        this.setState({ colour: colour.hex });
+    };
+
+    render() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={this.props.onCancel}
+            />,
+            <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onTouchTap={() => this.props.onSubmit(this.state.colour)}
+            />,
+        ];
+
+        return (
+            <div>
+                <Dialog
+                    title="Dialog With Actions"
+                    actions={actions}
+                    modal={false}
+                    open={this.props.isOpen}
+                    onRequestClose={this.props.onCancel}
+                >
+                    <BlockPicker
+                        color={ this.state.colour }
+                        onChangeComplete={this.handleChangeComplete}/>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+ColourDialog.propTypes = propTypes;
+
+export default ColourDialog;
--- src/header/header.js
+++ src/header/header.js
@@ -1,14 +1,78 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import ColourDialog from '../colourDialog/colourDialog';
 
-/**
- * A simple example of `AppBar` with an icon on the right.
- * By default, the left icon is a navigation-menu.
- */
-const AppBarExampleIcon = () => (
-    <AppBar
-        title="Chrono Dots"
-    />
-);
+class header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDrawerOpen: false,
+            isColourDialogOpen: false,
+            timeColours: {
+                fullDay: '#e57373',
+                halfDay: '#00e676',
+            },
+            colourPickerOpenId: ''
+        };
+    }
 
-export default AppBarExampleIcon;
+    handleToggle = () => this.setState({isDrawerOpen: !this.state.isDrawerOpen});
+
+    handleToggleIsColourDialogOpen = (id) => {
+        if(!this.state.isColourDialogOpen) {
+            this.setState({colourPickerOpenId: id});
+        } else {
+            this.setState({colourPickerOpenId: ''});
+        }
+        this.setState({isColourDialogOpen: !this.state.isColourDialogOpen});
+    };
+
+    handleClose = () => this.setState({isDrawerOpen: false});
+
+    handleSetColour = (colour) => {
+        let timeColours = this.state.timeColours;
+        timeColours[this.state.colourPickerOpenId] = colour;
+
+        this.handleToggleIsColourDialogOpen();
+        this.setState({timeColours});
+    };
+
+    render() {
+        const styles = {
+            fullDayStyle: {
+                backgroundColor: this.state.timeColours.fullDay,
+            },
+            halfDayStyle: {
+                backgroundColor: this.state.timeColours.halfDay,
+            }
+        };
+
+        return (
+            <div>
+                <AppBar
+                    title="Chrono Dots"
+                    onLeftIconButtonTouchTap={this.handleToggle}
+
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.isDrawerOpen}
+                    onRequestChange={(isDrawerOpen) => this.setState({isDrawerOpen})}
+                >
+                    <MenuItem onTouchTap={() => {this.handleClose(); this.handleToggleIsColourDialogOpen('fullDay')}} style={styles.fullDayStyle}>
+                        Full Day Colour
+                    </MenuItem>
+                    <MenuItem onTouchTap={() => {this.handleClose(); this.handleToggleIsColourDialogOpen('halfDay')}} style={styles.halfDayStyle}>
+                        Half Day Colour
+                    </MenuItem>
+                </Drawer>
+                <ColourDialog isOpen={this.state.isColourDialogOpen} onCancel={this.handleToggleIsColourDialogOpen} onSubmit={this.handleSetColour}/>
+            </div>
+        );
+    }
+}
+
+export default header;
