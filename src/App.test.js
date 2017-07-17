0f67d16e50c0828cd3117b802ccaf5514aa810f9
Logan Elliott <logansails@gmail.com>
2017-07-18 11:33:29 +1200

    Fix test. Add production-test. Add prepush hook

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
+
+injectTapEventPlugin();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App/>
+    </MuiThemeProvider>,
+    div);
 });
